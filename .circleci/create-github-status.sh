a40d6b1d052fbb412432169ae2de579c2557df92
Elisabeth Kulzer <elikul@elikul.de>
2019-12-19 14:14:27 +0100

    test

--- .circleci/create-github-status.sh
+++ .circleci/create-github-status.sh
@@ -1,10 +1,9 @@
 EXTERNAL_SHA=$1
 GITHUB_STATUS=$2
 GITHUB_DESCRIPTION=$3
-GITHUB_CONTEXT=$4
 curl \
   --request POST \
   --url https://api.github.com/repos/metanerd/circleci-test/statuses/"$EXTERNAL_SHA" \
   --user "$GITHUB_USER":"$GITHUB_USER_TOKEN" \
   --header 'content-type: application/json' \
-  --data "{\"state\": \"$GITHUB_STATUS\", \"description\": \"$GITHUB_DESCRIPTION\", \"context\": \"$GITHUB_CONTEXT\"}"
+  --data "{\"state\": \"$GITHUB_STATUS\", \"description\": \"$GITHUB_DESCRIPTION\", \"context\": \"ci-ee/circleci: $CIRCLE_JOB\"}"
