f79e80bc649dd1234852ba4029d0c3052f7f3d2f
Elisabeth Kulzer <elikul@elikul.de>
2019-12-19 13:26:38 +0100

    Refactor curl

---
+++ .circleci/create-github-status.sh
@@ -0,0 +1,12 @@
+EXTERNAL_SHA=$1
+GITHUB_STATUS=$2
+GITHUB_DESCRIPTION=$3
+GITHUB_CONTEXT=$4
+curl \
+  --request POST \
+  --url https://api.github.com/repos/metanerd/circleci-test/statuses/"$EXTERNAL_SHA" \
+  --user "$GITHUB_USER":"$GITHUB_USER_TOKEN" \
+  --header 'content-type: application/json' \
+  --data state=$GITHUB_STATUS \
+  --data description=$GITHUB_DESCRIPTION \
+  --data context=$GITHUB_CONTEXT
